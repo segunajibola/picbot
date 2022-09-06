55afcaca5b6bc417de1a2773589e51b0ef96db5c
Segun Ajibola <ajbl.segun@gmail.com>
2022-09-06 02:25:16 +0100

    dynamicPhotos

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 async function getPhotos() {
-  let response = await fetch("photo.json");
-  let photos = await response.json();
-
-  return photos;
+    let response = await fetch("photos.json")
+    let photos = await response.json()
+    return photos
 }
 
-getPhotos().then((photos) => {
-  let myPhotosHtml = photos.map((photo) => {
-      return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`;
-    })
-    .join("");
+function getMyPhotosHtml(photos) {
+    let myPhotosHtml = photos.map(photo => {
+        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
+    }).join('')
+    return `<div class="my-photos">${myPhotosHtml}</div>`
+}
 
-  console.log(myPhotosHtml);
-
-  document.body.innerHTML = `<div class="my-photos">${myPhotosHtml}</div>`;
-});
-
-// console.log(data)
+getPhotos().then(photos => {
+    let myPhotosHtml = getMyPhotosHtml(photos)
+    document.body.innerHTML = `<div class="my-gallery">
+        <img id="my-selected-photo" class="my-photo"
+        src="https://picsum.photos/id/${photos[2].id}/300/300" />
+        ${myPhotosHtml}
+    </div>`
+    
+    let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"))
+    myPhotoImgs.forEach(photoImg => {
+        photoImg.addEventListener("click", e => {
+            let selectedPhotoSrc = photoImg.src.substr(0, photoImg.src.length - 7) + `300/300`
+            let selectedPhoto = document.getElementById("my-selected-photo")
+            selectedPhoto.src = selectedPhotoSrc
+            selectedPhoto.style.display = "inline"
+        })
+    })    
+})
